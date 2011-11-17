a8dd0b23722611ef0c87c8df74fe7bbc34c47d83
russenreaktor <cr@russenreaktor.de>
2011-11-18 00:37:13 +0100

    moved weather icon 1px upper

--- src/org/metawatch/manager/Idle.java
+++ src/org/metawatch/manager/Idle.java
@@ -79,7 +79,7 @@ public class Idle {
 			
 			// icon
 			Bitmap image = Utils.loadBitmapFromAssets(context, WeatherData.icon);
-			canvas.drawBitmap(image, 36, 37, null);
+			canvas.drawBitmap(image, 36, 36, null);
 			
 			// temperatures
 			canvas.drawText(WeatherData.temp, 64, 46, paintLarge);				
