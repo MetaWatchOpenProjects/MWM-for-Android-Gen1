2e934501ea16cc918a686bb323970f0e4fec228c
Craig Oliver <great_staryu@hotmail.com>
2011-12-11 12:46:59 -0500

    Added support for Android 2.1.

--- src/org/metawatch/manager/MetaWatch.java
+++ src/org/metawatch/manager/MetaWatch.java
@@ -117,7 +117,7 @@ public class MetaWatch extends Activity {
     	
     	WebView webView = new WebView(this);
 		String html = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><title>About</title></head><body>" + 
-						"<p>Craig's Metawatch v1.1.6, based on:</p>" +
+						"<p>Craig's Metawatch v1.1.7, based on:</p>" +
 						"<h1>MetaWatch</h1>" +
 						"<p>Version " + Utils.getVersion(this) + ".</p>" +
 						"<p>Modified by Dobie Wollert, Chris Sewell, Prash D, benjymous, and Craig Oliver</p>" +
