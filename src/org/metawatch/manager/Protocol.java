a5ddaf4b513ee63d8ae8f430aac2e7e5f3817c2d
benjymous <rm.github@grapefruitopia.com>
2011-12-24 11:00:26 +0000

    Now actually sets 24/12hr mode depending on phone settings.  Credit to ChrisBoyle for spotting the function!

--- src/org/metawatch/manager/Protocol.java
+++ src/org/metawatch/manager/Protocol.java
@@ -292,6 +292,16 @@ public class Protocol {
 
 		} catch (Exception x) {
 		}
+		
+		// Set the watch to 12h or 24h mode, depending on watch setting
+		if (DateFormat.is24HourFormat(context)) {
+			Protocol.setNvalTime(true);
+			Log.d(MetaWatch.TAG, "Setting watch to 24h format");
+		}
+		else {
+			Protocol.setNvalTime(false);
+			Log.d(MetaWatch.TAG, "Setting watch to 12h format");
+		}
 	}
 
 	public static byte[] crc(byte[] bytes) {
