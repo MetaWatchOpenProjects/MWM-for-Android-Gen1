277f1af9070c336f502582db5dcc41b9b4515204
Kyle Schroeder <kyle.w.schroeder@gmail.com>
2012-02-17 13:48:01 -0600

    Just some minor tweaks

--- src/org/metawatch/manager/NotificationBuilder.java
+++ src/org/metawatch/manager/NotificationBuilder.java
@@ -136,7 +136,7 @@ public class NotificationBuilder {
 	public static void createCalendar(Context context, String text) {
 		VibratePattern vibratePattern = createVibratePatternFromPreference(context, "settingsCalendarNumberBuzzes");				
 		if (MetaWatchService.watchType == WatchType.DIGITAL) {
-			Bitmap bitmap = smartLines(context, "calendar.bmp", "Calendar", new String[] {text});	
+			Bitmap bitmap = smartLines(context, "calendar.bmp", "Calendar", new String[] {text + " which is being held at " + Utils.readCalendar(context, 0)});	
 			Notification.addBitmapNotification(context, bitmap, vibratePattern, Notification.getDefaultNotificationTimeout(context));	
 		} else {
 			byte[] scroll = new byte[800];
