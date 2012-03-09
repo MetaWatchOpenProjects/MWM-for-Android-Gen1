76f94f8ac9f6b5c793ff14bb569c32db876b96ba
Garth Bushell <garth@garthy.com>
2012-03-09 17:51:13 +0000

    on startup honour the autoconnect setting

--- src/org/metawatch/manager/BootCompletedReceiver.java
+++ src/org/metawatch/manager/BootCompletedReceiver.java
@@ -12,7 +12,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
-	     context.startService(new Intent(context, MetaWatchService.class));
+		 if(Preferences.autoConnect) {
+			 context.startService(new Intent(context, MetaWatchService.class));
+		 }
 	     if (Preferences.logging) Log.v(MetaWatch.TAG, "Service loaded at start");
 	}
 
