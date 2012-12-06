b4d3a3ac9291197c1c70ce70ad03dc2b011e76b2
Luis Ángel <siullegnak8@gmail.com>
2012-12-06 09:33:09 -0600

    Update src/org/metawatch/manager/Call.java

--- src/org/metawatch/manager/Call.java
+++ src/org/metawatch/manager/Call.java
@@ -59,8 +59,9 @@ public class Call {
 			Protocol.sendLcdBitmap(bitmap, MetaWatchService.WatchBuffers.NOTIFICATION);		
 			Protocol.updateDisplay(2);
 		} else 
+		{
 			Notification.addOledNotification(context, Protocol.createOled1line(context, "phone.bmp", "Call from"), Protocol.createOled1line(context, null, name), null, 0, new VibratePattern(true, 500, 500, 3));
-		
+		}
 		
 		Thread ringer = new Thread(new CallVibrate());
 		ringer.start();		
